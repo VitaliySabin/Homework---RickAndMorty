a460a7bcbc0b5a76953539e3e9371994cbb00ad5
Vitaliy Sbain <vitaliy-sabin@yandex.ru>
2023-05-07 16:31:39 +0300

    Work with model

--- Homework - RickAndMorty/RickAndMorty.swift
+++ Homework - RickAndMorty/RickAndMorty.swift
@@ -5,4 +5,27 @@
 //  Created by Виталий Сабин on 07.05.2023.
 //
 
-import Foundation
+struct RickAndMorty {
+    let results: [Character]
+}
+
+struct Character {
+    let name: String
+    let status: String
+    let species: String
+    let gender: String
+    let origin: Origin
+    let location: Location
+    let image: String
+    let episodes: [String]
+}
+
+struct Origin {
+    let name: String
+    let url: String
+}
+
+struct Location {
+    let name: String
+    let url: String
+}
