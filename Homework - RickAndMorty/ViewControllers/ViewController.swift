346f757fc29ae8d6e12916e18dba6a54890e14ba
Vitaliy Sbain <vitaliy-sabin@yandex.ru>
2023-05-07 16:41:24 +0300

    parsing JSON

--- Homework - RickAndMorty/ViewControllers/ViewController.swift
+++ Homework - RickAndMorty/ViewControllers/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let url = "https://rickandmortyapi.com/api/character"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fetchRickAndMorty()
     }
-
-
+    
+    private func fetchRickAndMorty() {
+        guard let url = URL(string: url) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let rickAndMorty = try decoder.decode(RickAndMorty.self, from: data)
+                print(rickAndMorty)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }.resume()
+    }
 }
 
