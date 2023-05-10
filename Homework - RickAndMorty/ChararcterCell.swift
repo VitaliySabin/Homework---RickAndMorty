2f47aef9db5d3c3262fafe8095c6b2a26b53e17d
Vitaliy Sbain <vitaliy-sabin@yandex.ru>
2023-05-10 20:31:24 +0300

    Work with connection of table view cell and network manager

--- Homework - RickAndMorty/ChararcterCell.swift
+++ Homework - RickAndMorty/ChararcterCell.swift
@@ -9,8 +9,27 @@ import UIKit
 
 final class CharacterCell: UITableViewCell {
     @IBOutlet var heroImage: UIImageView!
+    
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var statusLabel: UILabel!
     @IBOutlet var speciesLabel: UILabel!
     @IBOutlet var genderLabel: UILabel!
+    
+    private let networkManager = NetworkManager.shared
+    
+    private func configure(with character: Character) {
+        nameLabel.text = character.name
+        statusLabel.text = character.status
+        speciesLabel.text = character.species
+        genderLabel.text = character.gender
+        
+        networkManager.fetchImage(from: character.image) { [weak self] result in
+            switch result {
+            case .success(let imageData):
+                self?.heroImage.image = UIImage(data: imageData)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 }
