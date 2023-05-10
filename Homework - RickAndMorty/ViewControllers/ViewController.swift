04fed9d038c88ddf7e9295f51ac9516f6283bb04
Vitaliy Sbain <vitaliy-sabin@yandex.ru>
2023-05-10 19:41:12 +0300

    Initial work with TableVC

--- Homework - RickAndMorty/ViewControllers/ViewController.swift
+++ Homework - RickAndMorty/ViewControllers/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     
     private let url = "https://rickandmortyapi.com/api/character"
     
