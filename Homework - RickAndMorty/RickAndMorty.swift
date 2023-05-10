4c163e35719ee7f663887a1375aa09f811f1761d
Vitaliy Sbain <vitaliy-sabin@yandex.ru>
2023-05-10 19:39:52 +0300

    Rework with model

--- Homework - RickAndMorty/RickAndMorty.swift
+++ Homework - RickAndMorty/RickAndMorty.swift
@@ -14,18 +14,5 @@ struct Character: Decodable {
     let status: String
     let species: String
     let gender: String
-    let origin: Origin
-    let location: Location
     let image: String
-    let episode: [String]
-}
-
-struct Origin: Decodable {
-    let name: String
-    let url: String
-}
-
-struct Location: Decodable {
-    let name: String
-    let url: String
 }
