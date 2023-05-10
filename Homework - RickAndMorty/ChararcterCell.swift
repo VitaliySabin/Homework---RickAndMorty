d9a8f73a541153f5deb4eba98c6835f09c990517
Vitaliy Sbain <vitaliy-sabin@yandex.ru>
2023-05-10 19:40:35 +0300

    Create table view cell

---
+++ Homework - RickAndMorty/ChararcterCell.swift
@@ -0,0 +1,16 @@
+//
+//  RickAndMortyCell.swift
+//  Homework - RickAndMorty
+//
+//  Created by Виталий Сабин on 10.05.2023.
+//
+
+import UIKit
+
+final class CharacterCell: UITableViewCell {
+    @IBOutlet var heroImage: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var statusLabel: UILabel!
+    @IBOutlet var speciesLabel: UILabel!
+    @IBOutlet var genderLabel: UILabel!
+}
